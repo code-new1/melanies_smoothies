f2c28d66229e0425bddc2cad302655e7c0796e97
code-new1 <sendmail2rgs@gmail.com>
2025-05-08 00:20:37 -0400

    Update streamlit_app.py
    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+streamlit.title('Smoothie Shoppee')
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
